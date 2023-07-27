7c1ccc915fb120d6b36e924c176e4abf0e215050
Jairzinho Rocha <jair.rochant@gmail.com>
2023-07-27 22:13:48 +0000

    Atualização aula 5

--- src/main/java/com/example/AppTeste.java
+++ src/main/java/com/example/AppTeste.java
@@ -5,6 +5,25 @@ public class AppTeste {
         System.out.println();
         System.out.println(2+2);
         System.out.println();
+
+    // tipo nome da variável;
+    int idade;
+    idade = 11;
+    byte idadeEmByte = (byte)idade;
+
+    // tudo em uma linha só;
+    int peso = 50;
+
+    System.out.println("Idade é " + idadeEmByte);
+    System.out.println("Idade é " + idade);
+
+    System.out.println("Peso é " + peso);
+
+    byte gato = (byte)256;
+
+    System.out.println("Tenho essa quantidade de gatos: " + gato);
+
+
     }
     
 }
