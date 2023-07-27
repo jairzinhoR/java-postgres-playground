5ede305fe4cef6eecaf176902476857f3444658c
Jairzinho Rocha <jair.rochant@gmail.com>
2023-07-27 01:44:36 +0000

    Adicionado AppTeste Final

--- src/main/java/com/example/AppTeste.java
+++ src/main/java/com/example/AppTeste.java
@@ -3,10 +3,8 @@ package com.example;
 public class AppTeste {
     public static void main (String args[]) {
         System.out.println();
-        System.out.println("Teste final");
+        System.out.println(2+2);
         System.out.println();
-
-
     }
     
 }
