927eac47ab083fc3d06c7467e66608855f20edb5
Jairzinho Rocha <jair.rochant@gmail.com>
2023-07-28 17:51:36 +0000

    Aula16

---
+++ src/main/java/com/example/Calc4OperacoesGPT.java
@@ -0,0 +1,61 @@
+package com.example;
+import java.util.Scanner;
+
+public class Calc4OperacoesGPT {
+    public static void main(String[] args) {
+        
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Digite o símbolo da operação: + - * ou / ");
+        String operacao = scanner.next();
+        
+        // Usar equals() para comparar strings em Java, em vez do operador ==
+        if (operacao.equals("+")) {
+            operacao = "Adição";
+        } else if (operacao.equals("-")) {
+            operacao = "Subtração";
+        } else if (operacao.equals("*")) {
+            operacao = "Multiplicação";
+        } else if (operacao.equals("/")) {
+            operacao = "Divisão";
+        } else {
+            System.out.println("Operação inválida");
+            scanner.close();
+            return; // Encerra o programa em caso de operação inválida
+        }
+        
+        System.out.println("Você escolheu a operação: " + operacao);
+        
+        // Informe o primeiro número da operação
+        System.out.println("Informe o primeiro número da operação");
+        int num1 = scanner.nextInt();
+        System.out.println("Informe o segundo número da operação");
+        int num2 = scanner.nextInt();
+
+        // Cálculo
+        int result = 0; // Inicialize a variável result com um valor padrão
+
+        if (operacao.equals("Adição")) {
+            result = num1 + num2;
+    
+        } else if (operacao.equals("Subtração")) {
+            result = num1 - num2;
+    
+        } else if (operacao.equals("Multiplicação")) {
+            result = num1 * num2;
+
+        } else if (operacao.equals("Divisão")) {
+            if (num2 != 0) {
+                result = num1 / num2;
+            } else {
+                System.out.println("Divisão por zero não é permitida");
+                scanner.close();
+                return; // Encerra o programa em caso de divisão por zero
+            }
+        }
+
+        System.out.println("O resultado da operação é: " + result);
+
+        // Feche o Scanner para liberar recursos
+        scanner.close();
+    }
+}
---
+++ src/main/java/com/example/CalcMedia.java
@@ -0,0 +1,35 @@
+package com.example;
+import java.util.Scanner;
+
+public class CalcMedia {
+    
+    public static void main(String[] args) {
+   
+    // Criar um objeto Scanner para ler a entrada do usuário
+    Scanner scanner = new Scanner(System.in);
+    
+    // Solicite ao usuário que digite os três números
+    System.out.print("Digite o primeiro número: ");
+    int num1 = scanner.nextInt();
+
+    // Solicite ao usuário que digite os três números
+    System.out.print("Digite o segundo número: ");
+    int num2 = scanner.nextInt();
+
+    // Solicite ao usuário que digite os três números
+    System.out.print("Digite o terceiro número: ");
+    int num3 = scanner.nextInt();
+        
+    // Calcule a média dos três números
+    double media = (num1+num2+num3)/3.0;
+
+    // Imprima o resultado da média
+        System.out.println();
+        System.out.println("A média dos três números é: " + media);
+        System.out.println();
+    // Feche o Scanner para liberar recursos
+    scanner.close();
+        
+    }
+
+}
