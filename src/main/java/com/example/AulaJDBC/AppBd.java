adfe15a05a79d8b4b20ad5c0a8ab002b7f7b321d
Jairzinho Rocha <jair.rochant@gmail.com>
2023-08-07 12:20:00 +0000

    Com o AppBd

---
+++ src/main/java/com/example/AulaJDBC/AppBd.java
@@ -0,0 +1,27 @@
+package com.example.AulaJDBC;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+public class AppBd {
+
+    
+    public static void main(String[] args) {
+        try {
+           Class.forName("org.postgresql.Driver");
+           Connection conn = DriverManager.getConnection("jdbc:postgresql://localhost/postgres", "gitpod", "");
+           System.out.println("Conexão realizada com sucesso");
+           
+           var statement = conn.createStatement();
+           var result = statement.executeQuery("select * from estado");
+           while(result.next()){
+            System.out.printf("Id: %d Nome: %s UF: %s\n", result.getInt("id"), result.getString("nome"), result.getString("uf") );
+           }
+        } catch (ClassNotFoundException e) {
+            System.err.println("Não foi possível carregar a biblioteca/classe para acesso ao BD" + e.getMessage());   
+        } catch (SQLException e) {
+            System.err.println("Não foi possível conectar ao banco de dados" + e.getMessage());
+        }
+
+    }
+    
+}
