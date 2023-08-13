9aeedee7fb4605753a245fe4ec0cc9f057043616
Jairzinho Rocha <jair.rochant@gmail.com>
2023-08-13 10:55:04 +0000

    Conta.java

---
+++ src/main/java/com/example/teoria/Conta.java
@@ -0,0 +1,34 @@
+package com.example.teoria;
+
+public class Conta {
+    public int saldo;
+
+    public Conta(){
+    }
+
+    public int saldo(){
+        return saldo;
+    }
+
+    public int saca(int valor){
+        int saldoAtual = this.saldo - valor;
+        this.saldo = saldoAtual;
+        return this.saldo;
+    }
+
+    public int deposito(int valor){
+        int saldoAtual = this.saldo + valor;
+        this.saldo = saldoAtual;
+        return this.saldo;
+    }
+
+    public static void main(String[] args) {
+        Conta conta = new Conta();
+        System.out.println(conta.saldo());
+        conta.deposito(7500);
+        System.out.println(conta.saldo());
+        conta.saca(4500);
+        System.out.println(conta.saldo());
+    }
+
+}
