ba8d22e6e62b563c1117655e84a81eac0ddebb2d
Jairzinho Rocha <jair.rochant@gmail.com>
2023-08-15 09:44:06 +0000

    Worker

--- src/main/java/com/example/teoria/worker/Worker.java
+++ src/main/java/com/example/teoria/worker/Worker.java
@@ -1,5 +1,6 @@
 package com.example.teoria.worker;
 import java.util.List;
+import java.util.ArrayList;
 
 public class Worker {
     private String name;
@@ -7,8 +8,15 @@ public class Worker {
     private Double baseSalary;
 
     private Departament departament;
-    private List<HourContract> contracts;
+    private List<HourContract> contracts = new ArrayList<>();
     
+    public Worker(String name, WorkerLevel level, Double baseSalary, Departament departament) {
+        this.name = name;
+        this.level = level;
+        this.baseSalary = baseSalary;
+        this.departament = departament;
+    }
+
     public Worker(){
     }
 
@@ -17,11 +25,49 @@ public class Worker {
     }
 
     public void removeContract(HourContract contract){
-
     }
 
     public Double(int year, int month){
+    }
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public WorkerLevel getLevel() {
+        return level;
+    }
+
+    public void setLevel(WorkerLevel level) {
+        this.level = level;
+    }
+
+    public Double getBaseSalary() {
+        return baseSalary;
+    }
+
+    public void setBaseSalary(Double baseSalary) {
+        this.baseSalary = baseSalary;
+    }
+
+    public Departament getDepartament() {
+        return departament;
+    }
+
+    public void setDepartament(Departament departament) {
+        this.departament = departament;
+    }
+
+    public List<HourContract> getContracts() {
+        return contracts;
+    }
+
+    public void setContracts(List<HourContract> contracts) {
+        this.contracts = contracts;
     }
 
 }
