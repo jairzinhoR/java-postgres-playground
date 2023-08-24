515c971e238478be6caebd09242d18211e8764ff
Jairzinho Rocha <jair.rochant@gmail.com>
2023-08-24 03:20:59 +0000

    sqlite2

---
+++ src/main/java/com/example/python/sqlite2/readInsert.py
@@ -0,0 +1,39 @@
+import sqlite3
+
+try:
+    banco = sqlite3.connect('terc_banco.db')
+    cursor = banco.cursor()
+
+    # INSERT
+    #cursor.execute('''CREATE TABLE pessoas (
+    #    ID INTEGER PRIMARY KEY AUTOINCREMENT,
+    #    nome TEXT,
+    #    idade INTEGER,
+    #    email TEXT)''')
+    
+    #cursor.execute("INSERT INTO pessoas VALUES ('1','Jair',35,'jair.rochant@gmail.com')")
+    #cursor.execute("INSERT INTO pessoas VALUES('2', 'Dora', 66, 'dora@gmail.com')")
+    #cursor.execute("INSERT INTO pessoas VALUES('3','Jair Pai', 64, 'jrof@gmail.com')")
+    #cursor.execute("INSERT INTO pessoas VALUES('Laercio', 37, 'jkianne@gmail.com')")
+    #cursor.execute("INSERT INTO pessoas VALUES('Jessica', 32, 'laerciofan@gmail.com')")
+
+    #INSERT COM VARIÁVEIS
+    #nome = "Amelie"
+    #idade = 11
+    #email = "amelie@gmail.com"
+    #cursor.execute("INSERT INTO pessoas VALUES('"+nome+"',"+str(idade)+",'"+email+"')")
+
+    #READ
+    #cursor.execute("SELECT * FROM pessoas")
+    #print(cursor.fetchall())
+
+    #GOOD READ
+    pessoas = cursor.execute("SELECT * FROM pessoas")
+    for pessoa in pessoas:
+        print(pessoa)
+
+    banco.commit()
+    banco.close()
+
+except sqlite3.Error as erro:
+    print("Erro ao inserir dados: ", erro)
